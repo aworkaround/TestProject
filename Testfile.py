8c596bae0e28f98f25a03512688c4f039567b494
kamal2k89 <kamal@techaware.in>
2020-02-23 11:34:49 +0530

    Version 1.0.1 of progam is uploaded

--- Testfile.py
+++ Testfile.py
@@ -1,9 +1,10 @@
 # A simple program!
-# version -- 1.0.0
+# version -- 1.0.1
 
 name = input("Enter you name: ")
-print ("Hello, " + name[0].upper() + name[1:])
-
+mylist = name.split()
+print("Hello, " + mylist[0][0].upper() + mylist[0]
+      [1:].lower() + mylist[1][0].upper() + mylist[1][1:].lower())
 
 
 # End of simple program!
